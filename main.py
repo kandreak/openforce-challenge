1058928f9d7ebec7ecd1f883f5ed22f1c04faf32
k <andrea.piermarteri@gmail.com>
2020-07-20 11:26:19 +0200

    refined with some prints.

--- main.py
+++ main.py
@@ -19,8 +19,14 @@ repo = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/"
 #building url:
 url = repo + filename
 
+print("retrieving JSON at url " + url + " ... ")
 JSONString = WebRetriever.getJson(url)
+print("... done!")
+print("")
+
 JSONDict = JSONManager.parse(JSONString)
+
+print("filtering, aggregation and sorting of JSON data...")
 JSONDict = JSONManager.filterByDate(JSONDict, date)
 JSONDictRegione = JSONManager.getTotaleRegionale(JSONDict)
 
@@ -28,6 +34,15 @@ JSONDictRegione = JSONManager.getTotaleRegionale(JSONDict)
 #since i need ascending order for denominazione_regione and descending order for totale casi,
 #I do ascending order for both, but I negate each totale casi! :D
 JSONDictSorted = sorted(JSONDictRegione, key = lambda i: ((i["totale_casi"]*-1), i["denominazione_regione"]))
+print("... done!")
+print("")
 
+print("JSON content as a list of dictionaries:")
 for x in JSONDictSorted:
     print(x)
+
+JSONResult = JSONManager.serialize(JSONDictSorted)
+
+print("")
+print("JSON file content:")
+print(JSONResult)
